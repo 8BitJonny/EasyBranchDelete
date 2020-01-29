9447160f9e2c1b758cac78474961d96f469e8a8f
8BitJonny <timon99@gmx.de>
2020-01-29 10:44:33 +0100

    first workin version

--- index.js
+++ index.js
@@ -4,8 +4,33 @@ const exec = util.promisify(require('child_process').exec);
 
 async function main() {
   const { stdout, stderr } = await exec('git branch');
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
+  if (stderr) {	
+    console.log("🔥 Exited because git branch errored with: \n🔥", stderr)
+    return;
+  }
+  
+  const branches = stdout.split(/[\n\s*]+/).map(el => el.trim()).filter(el => el !== '')
+  
+  inquirer
+    .prompt([
+      {
+        type: "checkbox",
+        name: "branchToDelete",
+        message: "Select which branches to delete.",
+        choices: branches
+      }
+    ])
+    .then(answers => {
+      answers.branchToDelete.forEach(branch => {
+	exec(`git branch -d ${branch}`)
+          .then(({ stderr, stdout }) => {
+            if (stderr) console.log(`🔥 Couldn't delete branch because it thre error: \n${stderr}`);
+            console.log(stdout);
+          })
+          .catch(console.err)
+      })
+    })
+    .catch(err => console.error);
 }
 main();
 
